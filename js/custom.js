525ce1dae57e36c88521167bcb5c61260a207fac
seema poonia <seema.127poonia@gmail.com>
2019-07-25 18:07:56 +0530

    commit

---
+++ js/custom.js
@@ -0,0 +1,21 @@
+ $(document).ready(function(){
+
+$('#customers,#growth,#connected_life,#experience,#lifestyle').on('click', function() {
+  $.fancybox.open( $('.learn_more_gallery'), {
+    touch: false,
+    infobar: false
+  });
+});
+$('#continue_reading_img,#continue_reading_video').on('click', function() {
+  $.fancybox.open( $('.continue_reading'), {
+    touch: false,
+    infobar: false
+  });
+});
+  $('.truncate_word').each(function (f) {
+
+      var newstr = $(this).text().substring(0,250);
+      $(this).text(newstr);
+
+    });
+})
